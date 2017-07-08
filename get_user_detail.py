b143f9efb9344bf538c95fc979e15cf00659f4e2
Yogesh Kumar <yogesh.biebz@gmail.com>
2017-07-08 11:28:35 +0530

    Meaningful error message displayed when error is occured.

--- get_user_detail.py
+++ get_user_detail.py
@@ -2,16 +2,20 @@ from const import BASE_URL, APP_ACCESS_TOKEN
 import requests
 import get_user_id
 user_id = get_user_id.get_user_id()
-req_url = BASE_URL + "users/" + user_id + "/?access_token=" + APP_ACCESS_TOKEN
+if user_id != 0:
+    req_url = BASE_URL + "users/" + user_id + "/?access_token=" + APP_ACCESS_TOKEN
+else:
+    exit()
 print "request: " + req_url
 data = requests.get(req_url)
 data = data.json()
 if data['meta']['code'] == 200:
-    user_name = data['data']['full_name']
-    print "Full Name: %s" % user_name
-    user_username = data['data']['username']
-    print "Username: @%s" % user_username
-    user_bio = data['data']['bio']
-    print "Bio: %s" % user_bio
+    if len(data['data'])>0 :
+        user_name = data['data']['full_name']
+        print "Full Name: %s" % user_name
+        user_username = data['data']['username']
+        print "Username: @%s" % user_username
+        user_bio = data['data']['bio']
+        print "Bio: %s" % user_bio
 else:
     print "Something Went Wrong. :( "
--- get_user_id.py
+++ get_user_id.py
@@ -6,6 +6,10 @@ req_url = BASE_URL + "users/search?q=" + q + "&access_token=" + APP_ACCESS_TOKEN
 
 def get_user_id():
     user = requests.get(req_url).json()
-    user_username = user['data'][0]['username']
-    print user_username
-    return user['data'][0]['id']
+    if not user['data']:
+        print "No users found."
+    else:
+        user_username = user['data'][0]['username']
+        print user_username
+        return user['data'][0]['id']
+    return 0
