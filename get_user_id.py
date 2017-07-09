d5644486b794d7a0abc8700ed23beb860e9dec1d
Yogesh Kumar <yogesh.biebz@gmail.com>
2017-07-09 12:57:33 +0530

    Validation Bugs fixed

--- get_user_id.py
+++ get_user_id.py
@@ -1,10 +1,12 @@
 from const import BASE_URL, APP_ACCESS_TOKEN
+from validate import validate_char
 import requests
 
 
 def get_user_username():
 
     q = raw_input("Search user by name: ")
+    q = validate_char(q)
     req_url = BASE_URL + "users/search?q=" + q + "&access_token=" + APP_ACCESS_TOKEN
 
     user = requests.get(req_url).json()
