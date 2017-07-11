243a6699039afcdf6aef3e6a7ba5d6255e54dc88
Yogesh Kumar <yogesh.biebz@gmail.com>
2017-07-11 15:14:31 +0530

    Bug fixes in get_recent_media

--- get_recent_media.py
+++ get_recent_media.py
@@ -9,7 +9,7 @@ from const import BASE_URL, APP_ACCESS_TOKEN
 
 
 def get_recent_media(user_id):
-
+    req_url = ""
     if user_id != 0:
         req_url = BASE_URL + "users/" + user_id + "/media/recent/?access_token=" + APP_ACCESS_TOKEN
     else:
@@ -22,7 +22,11 @@ def get_recent_media(user_id):
     query = raw_input("Get post by searching for keyword in caption(leave empty for latest): ")
 
     if not query:
-        print "caption of post: " + data['data'][0]['caption']['text']
+        if len(data['data'][0]['caption']['text']) > 0:
+            print "caption of post: " + data['data'][0]['caption']['text']
+        else:
+            print "No Caption."
+
         img_url = data['data'][0]['images']['standard_resolution']['url']
         urllib.urlretrieve(img_url, 'hey.jpg')
         return data['data'][0]['id']
@@ -36,7 +40,10 @@ def get_recent_media(user_id):
         else:
             count += 1
 
-        print "caption of post: " + data['data'][count]['caption']['text']
+        if len(data['data'][count]['caption']['text']) > 0:
+            print "caption of post: " + data['data'][count]['caption']['text']
+        else:
+            print "No Caption."
         img_url = data['data'][count]['images']['standard_resolution']['url']
         urllib.urlretrieve(img_url, 'hey.jpg')
         return data['data'][count]['id']
